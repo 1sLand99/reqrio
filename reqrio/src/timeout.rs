973b0d7f231bafe177a2beb55b087e294c84089e
win11 <xinlanxi2017@gmail.com>
2026-01-08 01:06:10 +0800

    change timeout to millis

--- reqrio/src/timeout.rs
+++ reqrio/src/timeout.rs
@@ -21,10 +21,10 @@ pub struct Timeout {
 impl Timeout {
     pub fn new() -> Timeout {
         Timeout {
-            connect: Duration::from_secs(3),
-            read: Duration::from_secs(3),
-            write: Duration::from_secs(3),
-            handle: Duration::from_secs(30),
+            connect: Duration::from_millis(3000),
+            read: Duration::from_millis(3000),
+            write: Duration::from_millis(3000),
+            handle: Duration::from_millis(30000),
             connect_times: 3,
             handle_times: 3,
         }
@@ -60,20 +60,20 @@ impl Timeout {
         self.handle_times
     }
 
-    pub fn set_connect(&mut self, connect: u64) {
-        self.connect = Duration::from_secs(connect);
+    pub fn set_connect(&mut self, millis: u64) {
+        self.connect = Duration::from_secs(millis);
     }
 
-    pub fn set_read(&mut self, read: u64) {
-        self.read = Duration::from_secs(read);
+    pub fn set_read(&mut self, millis: u64) {
+        self.read = Duration::from_secs(millis);
     }
 
-    pub fn set_write(&mut self, write: u64) {
-        self.write = Duration::from_secs(write);
+    pub fn set_write(&mut self, millis: u64) {
+        self.write = Duration::from_secs(millis);
     }
 
-    pub fn set_handle(&mut self, handle: u64) {
-        self.handle = Duration::from_secs(handle);
+    pub fn set_handle(&mut self, millis: u64) {
+        self.handle = Duration::from_secs(millis);
     }
 
     pub fn set_connect_times(&mut self, connect_times: i32) {
