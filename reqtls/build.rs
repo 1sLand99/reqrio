7301012c74b8fc99fd876b905d7ccfec004288de
XLX <xinlanxi2017@gmail.com>
2026-01-26 01:34:38 +0800

    fix reqtls build

--- reqtls/build.rs
+++ reqtls/build.rs
@@ -1,5 +1,8 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-link-search=reqtls/lib");
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!("cargo:rustc-link-search={}/lib", manifest_dir);
     println!("cargo:rustc-link-lib=crypto");
     println!("cargo:rustc-link-lib=advapi32");
 }
