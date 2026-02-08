b62903e1cf962e4775d4ab51229d4cd9ecb375f0
xl <xinlanxi2017@gmail.com>
2026-02-09 01:00:40 +0800

    add br/deflate/zstd decode

--- reqrio/src/coder/mod.rs
+++ reqrio/src/coder/mod.rs
@@ -1,6 +1,6 @@
 
 use crate::error::HlsResult;
-use flate2::read::{DeflateDecoder, GzDecoder, GzEncoder};
+use flate2::read::{DeflateDecoder, DeflateEncoder, GzDecoder, GzEncoder};
 pub use hpack::*;
 use std::io::{BufReader, Read};
 use flate2::Compression;
@@ -39,20 +39,20 @@ pub fn chunk_decode(mut raw:Vec<u8>) -> HlsResult<Vec<u8>> {
 }
 
 
-// pub fn br_encode(brd: impl AsRef<[u8]>) -> HlsResult<Vec<u8>> {
-//     let params = brotli::enc::BrotliEncoderParams::default();
-//     let mut bufread = BufReader::new(brd.as_ref());
-//     let mut out = vec![];
-//     brotli::BrotliCompress(&mut bufread, &mut out, &params)?;
-//     Ok(out)
-// }
+pub fn br_encode(brd: impl AsRef<[u8]>) -> HlsResult<Vec<u8>> {
+    let params = brotli::enc::BrotliEncoderParams::default();
+    let mut bufread = BufReader::new(brd.as_ref());
+    let mut out = vec![];
+    brotli::BrotliCompress(&mut bufread, &mut out, &params)?;
+    Ok(out)
+}
 
-// pub fn deflate_encode(ded: impl AsRef<[u8]>) -> HlsResult<Vec<u8>> {
-//     let mut de = DeflateEncoder::new(ded.as_ref(), Compression::default());
-//     let mut out = vec![];
-//     de.read_to_end(&mut out)?;
-//     Ok(out)
-// }
+pub fn deflate_encode(ded: impl AsRef<[u8]>) -> HlsResult<Vec<u8>> {
+    let mut de = DeflateEncoder::new(ded.as_ref(), Compression::default());
+    let mut out = vec![];
+    de.read_to_end(&mut out)?;
+    Ok(out)
+}
 
 pub fn deflate_decode(ded: impl AsRef<[u8]>) -> HlsResult<Vec<u8>> {
     let mut de = DeflateDecoder::new(ded.as_ref());
@@ -69,22 +69,17 @@ pub fn gzip_encode(ded: impl AsRef<[u8]>) -> HlsResult<Vec<u8>> {
 }
 
 pub fn gzip_decode(ded: impl AsRef<[u8]>) -> HlsResult<Vec<u8>> {
-    if ded.as_ref().len() == 0 { return Ok(vec![]); }
+    if ded.as_ref().is_empty() { return Ok(vec![]); }
     let mut gd = GzDecoder::new(ded.as_ref());
     let mut out = vec![];
     gd.read_to_end(&mut out)?;
     Ok(out)
 }
 
-// pub fn zstd_encode(zst: impl AsRef<[u8]>) -> HlsResult<Vec<u8>> {
-//     Ok(zstd::encode_all(zst.as_ref(), zstd::DEFAULT_COMPRESSION_LEVEL)?)
-// }
+pub fn zstd_encode(zst: impl AsRef<[u8]>) -> HlsResult<Vec<u8>> {
+    Ok(zstd::encode_all(zst.as_ref(), zstd::DEFAULT_COMPRESSION_LEVEL)?)
+}
 
 pub fn zstd_decode(zst: impl AsRef<[u8]>) -> HlsResult<Vec<u8>> {
     Ok(zstd::decode_all(zst.as_ref())?)
 }
-
-// pub fn md5(content: impl AsRef<[u8]>) -> String {
-//     let res = md5::compute(content.as_ref()).to_vec();
-//     hex::encode(&res)
-// }
