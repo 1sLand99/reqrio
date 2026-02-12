7d8b82bc70b69c0f0af53af50b8bb11f7482dae9
xl <xinlanxi2017@gmail.com>
2026-02-12 17:56:15 +0800

    support link to dylib

--- reqtls/build.rs
+++ reqtls/build.rs
@@ -4,18 +4,12 @@ fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap();
-    match (target_os.as_ref(), target_env.as_ref()) {
-        ("windows", "msvc") => {
-            println!("cargo:rustc-link-search={}/lib", manifest_dir);
-            println!("cargo:rustc-link-lib=crypto_win_msvc");
-            println!("cargo:rustc-link-lib=zstd_win_msvc");
-            println!("cargo:rustc-link-lib=advapi32");
-        }
-        ("linux", "gnu") => {
-            println!("cargo:rustc-link-search={}/lib", manifest_dir);
-            println!("cargo:rustc-link-lib=crypto_linux_gnu");
-            println!("cargo:rustc-link-lib=zstd_linux_gnu");
-        }
-        _ => {}
+    let typ = if cfg!(feature = "dylib") { "dylib" } else { "static" };
+    if typ == "static" { println!("cargo:rustc-link-search={}/lib/{}/{}", manifest_dir, target_os, target_env); }
+    println!("{}", typ);
+    if target_os == "windows" {
+        println!("cargo:rustc-link-lib=advapi32");
     }
+    println!("cargo:rustc-link-lib={}=crypto", typ);
+    println!("cargo:rustc-link-lib={}=zstd", typ);
 }
