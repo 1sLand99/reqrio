00a145e79e2ab081614054b1f2a96064605190a8
xl <xinlanxi2017@gmail.com>
2026-02-27 15:11:22 +0800

    move RsaKey to key file

---
+++ reqtls/src/boring/rsa/key.rs
@@ -0,0 +1,123 @@
+use std::path::Path;
+use std::ptr::null_mut;
+use std::{fs, slice};
+use crate::boring::bindings::{EVP_PKEY_new, EVP_PKEY};
+use crate::boring::BoringResExt;
+use crate::boring::rsa::bindings::*;
+use crate::error::RlsResult;
+use crate::ffi::{BufPtr, CPointer};
+use crate::RlsError;
+use crate::RlsError::WritePubKeyError;
+
+pub struct RsaKey(CPointer<EVP_PKEY>);
+
+impl RsaKey {
+    pub fn none() -> RsaKey {
+        RsaKey(CPointer::nullptr())
+    }
+
+    fn new(mut rsa: CPointer<RSA>) -> RlsResult<RsaKey> {
+        let pkey = CPointer::new_checked(unsafe { EVP_PKEY_new() }, RlsError::PkeyNewError)?;
+        unsafe { EVP_PKEY_assign_RSA(pkey.as_mut_ptr(), rsa.as_mut_ptr()) }.ok(RlsError::PkeyAssignError)?;
+        rsa.disable_auto_free();
+        Ok(RsaKey(pkey))
+    }
+    pub fn gen_new_key(bits: i32) -> RlsResult<RsaKey> {
+        let rsa = CPointer::new_checked(unsafe { RSA_new() }, RlsError::RsaNewError)?;
+        let e = CPointer::new_checked(unsafe { BN_new() }, RlsError::BnNewError)?;
+        unsafe { BN_set_word(e.as_mut_ptr(), RSA_F4 as u64) }.ok(RlsError::BnSetWordError)?;
+        unsafe { RSA_generate_key_ex(rsa.as_mut_ptr(), bits, e.as_mut_ptr(), null_mut()) }.ok(RlsError::RsaGenKeyError)?;
+        RsaKey::new(rsa)
+    }
+
+    pub fn to_pri_pem(&self) -> RlsResult<String> {
+        let bio = CPointer::new_checked(unsafe { BIO_new(BIO_s_mem()) }, RlsError::BioNewError)?;
+        unsafe {
+            PEM_write_bio_PrivateKey(
+                bio.as_mut_ptr(),
+                self.0.as_mut_ptr(),
+                null_mut(),
+                null_mut(),
+                0,
+                None,
+                null_mut(),
+            )
+        }.ok(RlsError::WritePriKeyError)?;
+        let mut data = null_mut();
+        let len = unsafe { BIO_get_mem_data(bio.as_mut_ptr(), &mut data) };
+        let out = unsafe { slice::from_raw_parts(data as *const u8, len as usize) }.to_vec();
+        Ok(String::from_utf8(out)?)
+    }
+
+    pub fn to_pub_pem(&self) -> RlsResult<String> {
+        let bio = CPointer::new_checked(unsafe { BIO_new(BIO_s_mem()) }, RlsError::BioNewError)?;
+        unsafe { PEM_write_bio_PUBKEY(bio.as_mut_ptr(), self.0.as_mut_ptr()) }.ok(WritePubKeyError)?;
+        let mut data = null_mut();
+        let len = unsafe { BIO_get_mem_data(bio.as_mut_ptr(), &mut data) };
+        let out = unsafe { slice::from_raw_parts(data as *const u8, len as usize) }.to_vec();
+        Ok(String::from_utf8(out)?)
+    }
+
+    pub fn to_pri_der(&self) -> BufPtr {
+        let mut buf = BufPtr::nullptr();
+        let len = unsafe { i2d_PrivateKey(self.0.as_ptr(), buf.ptr_mut()) };
+        buf.set_len(len as usize);
+        buf
+    }
+
+    pub fn to_pub_der(&self) -> BufPtr {
+        let mut buf = BufPtr::nullptr();
+        let len = unsafe { i2d_PUBKEY(self.0.as_ptr(), buf.ptr_mut()) };
+        buf.set_len(len as usize);
+        buf
+    }
+
+    pub fn from_pri_pem(pem: impl AsRef<[u8]>) -> RlsResult<RsaKey> {
+        let bio = CPointer::new(unsafe { BIO_new_mem_buf(pem.as_ref().as_ptr() as *const _, pem.as_ref().len() as isize) });
+        if bio.is_null() { return Err(RlsError::BioNewError); }
+        let pkey = CPointer::new(unsafe { PEM_read_bio_PrivateKey(bio.as_mut_ptr(), null_mut(), None, null_mut()) });
+        if pkey.is_null() { return Err(RlsError::BioNewError); }
+        Ok(RsaKey(pkey))
+    }
+
+    pub fn from_pri_pem_file(pem_file: impl AsRef<Path>) -> RlsResult<RsaKey> {
+        let pem = fs::read(pem_file)?;
+        RsaKey::from_pri_pem(pem)
+    }
+
+    pub fn from_pub_pem(pem: impl AsRef<[u8]>) -> RlsResult<RsaKey> {
+        let bio = CPointer::new(unsafe { BIO_new_mem_buf(pem.as_ref().as_ptr() as *const _, pem.as_ref().len() as isize) });
+        if bio.is_null() { return Err(RlsError::BioNewError); }
+        let pkey = CPointer::new(unsafe { PEM_read_bio_PUBKEY(bio.as_mut_ptr(), null_mut(), None, null_mut()) });
+        if pkey.is_null() { return Err(RlsError::PkeyNewError); };
+        Ok(RsaKey(pkey))
+    }
+
+    pub fn from_pri_der(der: impl AsRef<[u8]>) -> RlsResult<RsaKey> {
+        let pkey = CPointer::new(unsafe { d2i_AutoPrivateKey(null_mut(), &mut der.as_ref().as_ptr(), (der.as_ref().len() as u16).into()) });
+        if pkey.is_null() { return Err(RlsError::PkeyNewError); };
+        Ok(RsaKey(pkey))
+    }
+
+    pub fn from_pub_der(der: impl AsRef<[u8]>) -> RlsResult<RsaKey> {
+        let pkey = CPointer::new(unsafe { d2i_PUBKEY(null_mut(), &mut der.as_ref().as_ptr(), (der.as_ref().len() as u16).into()) });
+        if pkey.is_null() { return Err(RlsError::PkeyNewError); };
+        Ok(RsaKey(pkey))
+    }
+
+    pub fn from_e_n(e: impl AsRef<[u8]>, n: impl AsRef<[u8]>) -> RlsResult<RsaKey> {
+        let e = hex::decode(e)?;
+        let n = hex::decode(n)?;
+        let mut e = CPointer::new_checked(unsafe { BN_bin2bn(e.as_ptr(), e.len(), null_mut()) }, RlsError::BnNewError)?;
+        let mut n = CPointer::new_checked(unsafe { BN_bin2bn(n.as_ptr(), n.len(), null_mut()) }, RlsError::BnNewError)?;
+        let rsa = CPointer::new_checked(unsafe { RSA_new() }, RlsError::RsaNewError)?;
+        unsafe { RSA_set0_key(rsa.as_mut_ptr(), n.as_mut_ptr(), e.as_mut_ptr(), null_mut()) }.ok(RlsError::RsaGenKeyError)?;
+        e.disable_auto_free();
+        n.disable_auto_free();
+        RsaKey::new(rsa)
+    }
+
+    pub fn pkey(&self) -> &CPointer<EVP_PKEY> {
+        &self.0
+    }
+}
--- reqtls/src/boring/rsa/mod.rs
+++ reqtls/src/boring/rsa/mod.rs
@@ -1,133 +1,22 @@
 use crate::boring::bindings::*;
 use crate::boring::BoringResExt;
 use crate::error::RlsResult;
-use crate::ffi::{BufPtr, CPointer};
+use crate::ffi::CPointer;
 use crate::RlsError;
-use crate::RlsError::WritePubKeyError;
 use bindings::*;
-use std::path::Path;
 use std::ptr::null_mut;
-use std::{fs, slice};
+pub use key::RsaKey;
+pub use padding::RsaPadding;
 
 pub mod certificate;
 #[allow(dead_code)]
 pub(crate) mod bindings;
-pub struct RsaKey(CPointer<EVP_PKEY>);
-
-impl RsaKey {
-    pub fn none() -> RsaKey {
-        RsaKey(CPointer::nullptr())
-    }
-
-    fn new(mut rsa: CPointer<RSA>) -> RlsResult<RsaKey> {
-        let pkey = CPointer::new_checked(unsafe { EVP_PKEY_new() }, RlsError::PkeyNewError)?;
-        unsafe { EVP_PKEY_assign_RSA(pkey.as_mut_ptr(), rsa.as_mut_ptr()) }.ok(RlsError::PkeyAssignError)?;
-        rsa.disable_auto_free();
-        Ok(RsaKey(pkey))
-    }
-    pub fn gen_new_key(bits: i32) -> RlsResult<RsaKey> {
-        let rsa = CPointer::new_checked(unsafe { RSA_new() }, RlsError::RsaNewError)?;
-        let e = CPointer::new_checked(unsafe { BN_new() }, RlsError::BnNewError)?;
-        unsafe { BN_set_word(e.as_mut_ptr(), RSA_F4 as u64) }.ok(RlsError::BnSetWordError)?;
-        unsafe { RSA_generate_key_ex(rsa.as_mut_ptr(), bits, e.as_mut_ptr(), null_mut()) }.ok(RlsError::RsaGenKeyError)?;
-        RsaKey::new(rsa)
-    }
-
-    pub fn to_pri_pem(&self) -> RlsResult<String> {
-        let bio = CPointer::new_checked(unsafe { BIO_new(BIO_s_mem()) }, RlsError::BioNewError)?;
-        unsafe {
-            PEM_write_bio_PrivateKey(
-                bio.as_mut_ptr(),
-                self.0.as_mut_ptr(),
-                null_mut(),
-                null_mut(),
-                0,
-                None,
-                null_mut(),
-            )
-        }.ok(RlsError::WritePriKeyError)?;
-        let mut data = null_mut();
-        let len = unsafe { BIO_get_mem_data(bio.as_mut_ptr(), &mut data) };
-        let out = unsafe { slice::from_raw_parts(data as *const u8, len as usize) }.to_vec();
-        Ok(String::from_utf8(out)?)
-    }
-
-    pub fn to_pub_pem(&self) -> RlsResult<String> {
-        let bio = CPointer::new_checked(unsafe { BIO_new(BIO_s_mem()) }, RlsError::BioNewError)?;
-        unsafe { PEM_write_bio_PUBKEY(bio.as_mut_ptr(), self.0.as_mut_ptr()) }.ok(WritePubKeyError)?;
-        let mut data = null_mut();
-        let len = unsafe { BIO_get_mem_data(bio.as_mut_ptr(), &mut data) };
-        let out = unsafe { slice::from_raw_parts(data as *const u8, len as usize) }.to_vec();
-        Ok(String::from_utf8(out)?)
-    }
-
-    pub fn to_pri_der(&self) -> BufPtr {
-        let mut buf = BufPtr::nullptr();
-        let len = unsafe { i2d_PrivateKey(self.0.as_ptr(), buf.ptr_mut()) };
-        buf.set_len(len as usize);
-        buf
-    }
-
-    pub fn to_pub_der(&self) -> BufPtr {
-        let mut buf = BufPtr::nullptr();
-        let len = unsafe { i2d_PUBKEY(self.0.as_ptr(), buf.ptr_mut()) };
-        buf.set_len(len as usize);
-        buf
-    }
-
-    pub fn from_pri_pem(pem: impl AsRef<[u8]>) -> RlsResult<RsaKey> {
-        let bio = CPointer::new(unsafe { BIO_new_mem_buf(pem.as_ref().as_ptr() as *const _, pem.as_ref().len() as isize) });
-        if bio.is_null() { return Err(RlsError::BioNewError); }
-        let pkey = CPointer::new(unsafe { PEM_read_bio_PrivateKey(bio.as_mut_ptr(), null_mut(), None, null_mut()) });
-        if pkey.is_null() { return Err(RlsError::BioNewError); }
-        Ok(RsaKey(pkey))
-    }
-
-    pub fn from_pri_pem_file(pem_file: impl AsRef<Path>) -> RlsResult<RsaKey> {
-        let pem = fs::read(pem_file)?;
-        RsaKey::from_pri_pem(pem)
-    }
-
-    pub fn from_pub_pem(pem: impl AsRef<[u8]>) -> RlsResult<RsaKey> {
-        let bio = CPointer::new(unsafe { BIO_new_mem_buf(pem.as_ref().as_ptr() as *const _, pem.as_ref().len() as isize) });
-        if bio.is_null() { return Err(RlsError::BioNewError); }
-        let pkey = CPointer::new(unsafe { PEM_read_bio_PUBKEY(bio.as_mut_ptr(), null_mut(), None, null_mut()) });
-        if pkey.is_null() { return Err(RlsError::PkeyNewError); };
-        Ok(RsaKey(pkey))
-    }
-
-    pub fn from_pri_der(der: impl AsRef<[u8]>) -> RlsResult<RsaKey> {
-        let pkey = CPointer::new(unsafe { d2i_AutoPrivateKey(null_mut(), &mut der.as_ref().as_ptr(), (der.as_ref().len() as u16).into()) });
-        if pkey.is_null() { return Err(RlsError::PkeyNewError); };
-        Ok(RsaKey(pkey))
-    }
-
-    pub fn from_pub_der(der: impl AsRef<[u8]>) -> RlsResult<RsaKey> {
-        let pkey = CPointer::new(unsafe { d2i_PUBKEY(null_mut(), &mut der.as_ref().as_ptr(), (der.as_ref().len() as u16).into()) });
-        if pkey.is_null() { return Err(RlsError::PkeyNewError); };
-        Ok(RsaKey(pkey))
-    }
-
-    pub fn from_e_n(e: impl AsRef<[u8]>, n: impl AsRef<[u8]>) -> RlsResult<RsaKey> {
-        let e = hex::decode(e)?;
-        let n = hex::decode(n)?;
-        let mut e = CPointer::new_checked(unsafe { BN_bin2bn(e.as_ptr(), e.len(), null_mut()) }, RlsError::BnNewError)?;
-        let mut n = CPointer::new_checked(unsafe { BN_bin2bn(n.as_ptr(), n.len(), null_mut()) }, RlsError::BnNewError)?;
-        let rsa = CPointer::new_checked(unsafe { RSA_new() }, RlsError::RsaNewError)?;
-        unsafe { RSA_set0_key(rsa.as_mut_ptr(), n.as_mut_ptr(), e.as_mut_ptr(), null_mut()) }.ok(RlsError::RsaGenKeyError)?;
-        e.disable_auto_free();
-        n.disable_auto_free();
-        RsaKey::new(rsa)
-    }
-
-    pub fn pkey(&self) -> &CPointer<EVP_PKEY> {
-        &self.0
-    }
-}
-
+mod key;
+mod padding;
 
 pub struct RsaCipher {
     ctx: CPointer<EVP_PKEY_CTX>,
+    padding: RsaPadding,
 }
 
 impl RsaCipher {
@@ -136,16 +25,16 @@ impl RsaCipher {
         if ctx.is_null() { return Err(RlsError::RsaNewError); }
         Ok(RsaCipher {
             ctx,
+            padding: RsaPadding::Pkcs1,
         })
     }
     pub fn from_rsa_key(key: &RsaKey) -> RlsResult<RsaCipher> {
-        RsaCipher::new(&key.0)
+        RsaCipher::new(key.pkey())
     }
 
-    pub fn encrypt(&self, data: impl AsRef<[u8]>, oaep: bool) -> RlsResult<Vec<u8>> {
+    pub fn encrypt(&self, data: impl AsRef<[u8]>) -> RlsResult<Vec<u8>> {
         unsafe { EVP_PKEY_encrypt_init(self.ctx.as_mut_ptr()) }.ok(RlsError::InitEncryptError)?;
-        let padding = if oaep { RSA_PKCS1_OAEP_PADDING } else { RSA_PKCS1_PADDING };
-        unsafe { EVP_PKEY_CTX_set_rsa_padding(self.ctx.as_mut_ptr(), padding) }.ok(RlsError::RsaSetPaddingError)?;
+        unsafe { EVP_PKEY_CTX_set_rsa_padding(self.ctx.as_mut_ptr(), self.padding.as_i32()) }.ok(RlsError::RsaSetPaddingError)?;
         let mut out_len = 0;
         unsafe {
             EVP_PKEY_encrypt(
@@ -169,10 +58,9 @@ impl RsaCipher {
         Ok(out)
     }
 
-    pub fn decrypt(&self, data: impl AsRef<[u8]>, oaep: bool) -> RlsResult<Vec<u8>> {
+    pub fn decrypt(&self, data: impl AsRef<[u8]>) -> RlsResult<Vec<u8>> {
         unsafe { EVP_PKEY_decrypt_init(self.ctx.as_mut_ptr()) }.ok(RlsError::InitDecryptError)?;
-        let padding = if oaep { RSA_PKCS1_OAEP_PADDING } else { RSA_PKCS1_PADDING };
-        unsafe { EVP_PKEY_CTX_set_rsa_padding(self.ctx.as_mut_ptr(), padding) }.ok(RlsError::RsaSetPaddingError)?;
+        unsafe { EVP_PKEY_CTX_set_rsa_padding(self.ctx.as_mut_ptr(), self.padding.as_i32()) }.ok(RlsError::RsaSetPaddingError)?;
         let mut out_len = data.as_ref().len();
         let mut out = vec![0u8; data.as_ref().len()];
         unsafe {
@@ -202,12 +90,12 @@ mod tests {
         println!("{:?}", key.to_pub_der());
         let nkey = RsaKey::from_pub_der(key.to_pub_der().as_slice()).unwrap();
         let rsa = RsaCipher::from_rsa_key(&nkey).unwrap();
-        let encrypted = rsa.encrypt("adsdfds", true).unwrap();
+        let encrypted = rsa.encrypt("adsdfds").unwrap();
         println!("{} {:?}", encrypted.len(), encrypted);
 
         let nkey = RsaKey::from_pri_der(key.to_pri_der().as_slice()).unwrap();
         let rsa = RsaCipher::from_rsa_key(&nkey).unwrap();
-        let decrypted = rsa.decrypt(encrypted.as_slice(), true).unwrap();
+        let decrypted = rsa.decrypt(encrypted.as_slice()).unwrap();
         println!("{} {:?}", decrypted.len(), decrypted);
     }
 }
