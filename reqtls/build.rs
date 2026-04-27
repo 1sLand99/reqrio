d607220169013a2bb598e2c32036145c6699e260
XLX <xinlanxi2017@gmail.com>
2026-04-27 17:02:32 +0800

    fix run with static link run error

--- reqtls/build.rs
+++ reqtls/build.rs
@@ -1,40 +1,21 @@
-use std::{env, fs};
-use std::path::{Path, PathBuf};
-use std::str::FromStr;
+use std::env;
 
 fn main() {
-
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap();
-    println!("cargo:rustc-link-search={}/lib/{}/{}", manifest_dir, target_os, target_env);
-    println!("cargo:rustc-link-lib=bcrypto");
-    println!("cargo:rustc-link-lib=zstd");
-    let mut dll_src = PathBuf::from_str(manifest_dir.as_str()).unwrap();
-    dll_src.push("lib");
-    dll_src.push(target_os.as_str());
-    dll_src.push(target_env.as_str());
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let path = Path::new(out_dir.as_str()).parent().unwrap().parent().unwrap().parent().unwrap();
-    let mut dll_dst = path.to_path_buf();
+    let typ = if cfg!(feature = "dylib") { "dylib" } else { "static" };
+    if typ == "static" { println!("cargo:rustc-link-search={}/lib/{}/{}", manifest_dir, target_os, target_env); }
+    println!("cargo:rustc-link-lib={}=bcrypto", typ);
+    println!("cargo:rustc-link-lib={}=zstd", typ);
+    // println!("cargo:rustc-link-lib=versus");
     match (target_os.as_str(), target_env.as_str()) {
-        ("windows", "msvc") => {
-            println!("cargo:rustc-link-lib=advapi32");
-            dll_src.push("bcrypto.dll");
-            dll_dst.push("bcrypto.dll")
-        }
+        ("windows", "msvc") => println!("cargo:rustc-link-lib=advapi32"),
         ("windows", "gnu") => {
             println!("cargo:rustc-link-lib=dylib=advapi32");
             println!("cargo:rustc-link-lib=stdc++");
-            dll_src.push("libbcrypto.dll");
-            dll_dst.push("libbcrypto.dll")
         }
-        ("linux", "gnu") => {
-            println!("cargo:rustc-link-lib=dylib=stdc++");
-            dll_src.push("libbcrypto.so");
-            dll_dst.push("libbcrypto.so")
-        }
+        ("linux", "gnu") => println!("cargo:rustc-link-lib=dylib=stdc++"),
         (_, _) => {}
     }
-    fs::copy(dll_src, dll_dst).unwrap();
 }
